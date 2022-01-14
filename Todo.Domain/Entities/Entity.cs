9002774a38bd765692f1062f5ada57cb3dd70b37
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-01-14 09:44:28 -0300

    initial commit

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities {
+    //não dá pra ser instanciada - abstract
+    //ela é a classe base, outras usarão e n faz sentido ser instanciada
+
+    public abstract class Entity : IEquatable<Entity>
+        {
+        public Guid Id { get; private set; }
+        public Entity (Guid id) {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(Entity other) {
+            return Id == other.Id;
+
+        }
+    }
+}
