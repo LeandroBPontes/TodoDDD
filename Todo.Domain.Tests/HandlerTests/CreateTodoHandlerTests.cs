6e0a8315d3c7570b2ea2eba3ac900225a0f7018d
Leandro Barros Pontes <65022896+LeandroBPontes@users.noreply.github.com>
2022-02-14 19:52:39 -0300

    testando handlers

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Handlers;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests {
     [TestClass]
     public class CreateTodoHandlerTests {
 
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da Tarefa", "Leandro", DateTime.Now);
+
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+
+        public CreateTodoHandlerTests() {}
+
         [TestMethod]
         public void Dado_um_comando_invalido_deve_interromper_execucao() {
-            Assert.Fail();
+
+            var command = new CreateTodoCommand("", "", DateTime.Now);
+         
+            var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(result.Success, false);
         }
         [TestMethod]
         public void Dado_um_comando_valido_deve_criar_tarefa() {
-            Assert.Fail();
+
+            var command = new CreateTodoCommand("Titulo da Tarefa", "Leandro", DateTime.Now);
+            
+            var result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(result.Success, true);
+           
         }
     }
 }
---
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Todo.Domain.Commands.Repositories;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.Repositories {
+    public class FakeTodoRepository : ITodoRepository {
+        public void Create(TodoItem todo) {
+         
+        }
+
+        public void Update(TodoItem todo) {
+            
+        }
+    }
+}
